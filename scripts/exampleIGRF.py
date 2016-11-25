93c98387b4fa8ba1212d791291037fde2e7e47c0
rilma <rri5@cornell.edu>
2016-11-25 11:06:32 -0500

    Python wrapper for the IGRF model

--- scripts/exampleIGRF.py
+++ scripts/exampleIGRF.py
@@ -1,26 +1,28 @@
 
 
-def batch_igrf():
+# def batch_igrf():
 
-    from pyigrf.pyigrf import IGRFSub
+#     from pyigrf.pyigrf import IGRFSub
 
-    xlat = -11.95
-    xlong = 283.13
-    year = 2004.75
-    height = 0.
+#     xlat = -11.95
+#     xlong = 283.13
+#     year = 2004.75
+#     height = 0.
 
-    xl, icode, dip, dec = IGRFSub(xlat, xlong, year, height)
+#     xl, icode, dip, dec = IGRFSub(xlat, xlong, year, height)
 
-    print( xl, icode, dip, dec)
+#     print( xl, icode, dip, dec)
 
-    #
-    # End of 'batch_igrf'
-    #####
+#     #
+#     # End of 'batch_igrf'
+#     #####
 
 
 def batch_get_igrf():
 
-    from pyigrf.pyigrf import GetIGRF 
+    from matplotlib.pyplot import figure, show
+    from pyigrf.pyigrf import GetIGRF
+    from scipy import arange 
 
     xlat = -11.95
     xlon = 283.13
@@ -36,9 +38,34 @@ def batch_get_igrf():
     #####
 
 
+# def heightProfile():
+
+#     from matplotlib.pyplot import figure, show
+#     from pyigrf.pyigrf import GetIGRF
+#     from scipy import arange, nan, tile 
+    
+#     xlat = -11.95
+#     xlon = 283.13
+#     year = 2004.75
+#     altlim = [0., 1000.]
+#     altstp = 50.
+    
+#     altbins = arange(altlim[0], altlim[1] + altstp, altstp)
+#     babs = tile(nan, len(altbins))
+#     for i in range(len(altbins)):
+#         bn, be, bd, xl, icode = GetIGRF(xlat, xlon, altbins[i], year)
+#         babs[i] = (bn**2 + be**2 + bd**2)**.5
+
+#     f = figure(figsize=(8,6))
+#     pn = f.add_subplot(111)
+#     pn.plot(babs, altbins, color='k')
+#     show()
+
+
 if __name__ == '__main__':
 
-    batch_igrf()
+    ##batch_igrf()
 
     batch_get_igrf()
 
+    #heightProfile()
